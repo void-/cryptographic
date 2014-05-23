297b0d874cf7fc9e5466c307fbde6675e29fcf1d
void- <mr.alex.guy@gmail.com>
2014-05-22 23:06:07 -0700

    crypto: Implemented Storer constructor; incomplete

--- src/com/key/Storer.java
+++ src/com/key/Storer.java
@@ -1,4 +1,8 @@
 package key;
+
+import android.content.Context;
+import java.security.*;
+
 /**
  *  Storer class handles storage and access of a single private key.
  *
@@ -17,11 +21,70 @@ package key;
  */
 public class Storer
 {
+  /**
+   *  Class Variables.
+   *
+   *  KEYBITS constant int representing the size of keys to use.
+   *  CIPHER constant string representing which public key algorithm to use.
+   */
+  public static final int KEYBITS = 1024;
+  public static final String CIPHER = "RSA";
+  private static final String KEYSTORENAME = ".privKeystore";
+
+  /**
+   *  Member Variables.
+   *
+   *  ks KeyStore for accessing private key.
+   */
+  private KeyStore ks;
+
+  /**
+   *  Storer() constructs a new storer instance.
+   *
+   *  If no private key currently exists, generate a public+private key pair
+   *  and store it. This will only happen once. If a private key already
+   *  exists, extract it from the key store.
+   *
+   *  Use null for password.
+   *  Use null for cert chain:certs aren't relevant
+   *
+   */
   Storer()
   {
-    //setup a connection to the database/ pull out key from keystore
+    this.ks = KeyStore.getInstance(KeyStore.getDefaultType());
+    try
+    {
+      //try to load private key from disk
+      FileInputStream f = openFileInput(Storer.KEYSTORENAME);
+      ks.load(f);
+      f.close();
+    }
+    catch(FileNotFoundException e)
+    {
+      //generate a new keypair for the user
+      KeyPairGenerator kgen = new KeyPairGenerator.getInstance(Storer.CIPHER);
+      kgen.initialize(Storer.KEYBITS);
+      KeyPair keyPair = kpg.generateKeyPair();
+
+      //store private key into keystore
+      ks.setKeyEntry(USERNUMBER, keyPair.getPrivate(), null, null);
+      //TODO:Go store KeyPair.public for my own number with fetcher
+        //what is my own number?
+
+      //write keystore to disk
+      FileOutputStream f = openFileOutput(Storer.KEYSTORENAME,
+        Context.MODE_PRIVATE);
+      ks.store(f, null);
+      f.close();
+    }
   }
 
+  /**
+   *  decrypt() given a ciphertext will decrypt it with the private key.
+   *
+   *  Unfortunately RSA is vulnerable to chosen ciphertext(oops), maybe this
+   *  shouldn't be a public method but rather package protected.
+   */
   public PLAINTEXT decrypt(CIPHERTEXT c)
   {
   }
