742fa107bac4799a092209a6d77ff01ee34a7cbb
void- <mr.alex.guy@gmail.com>
2014-05-29 21:49:05 -0700

    service: Implemented binary SMS receiver and decryption.

--- src/com/ctxt/CtxtService.java
+++ src/com/ctxt/CtxtService.java
@@ -1,10 +1,13 @@
 package com.ctxt;
 
-// What do I even include?
+import com.key.Key;
+
+import android.util.Log;
 import android.os.Bundle;
 import android.app.Service;
 import android.os.IBinder;
 import android.content.*;
+import android.telephony.SmsMessage;
 
 /**
  *  CtxtService service for receiving crypto SMS in the background.
@@ -39,8 +42,13 @@ public class CtxtService extends Service
   }
 
   /**
-   *  onBind(); I don't even know what this does.
+   *  onBind() is called when another process wants to communicate with the
+   *  CtxtService.
    *
+   *  There does not appear to be any reason why one would want to bind with
+   *  this service, so null is always returned.
+   *
+   *  @return null: binding to CtxtService is prohibited.
    */
   @Override
   public IBinder onBind(Intent intent)
@@ -55,20 +63,39 @@ public class CtxtService extends Service
    *  folder as plaintext. Access the private key for decryption.
    *
    *  If a plaintext SMS is received: What do?
+   *    A plaintext SMS should never be received due to port and intent
+   *    filtering.
    */
   private class SMSreceiver extends BroadcastReceiver
   {
+    private static final String ACTION_SMS_RECEIVED =
+      "android.provider.Telephony.SMS_RECEIVED";
+    private static String TAG = "smsSERVICE";
     /**
      *  onReceive() receives SMS and decrypts them using the private key.
      */
     @Override
     public void onReceive(Context context, Intent intent)
     {
+      //abort if the intent is for receiving an sms: ?Non-binary => plaintext
+      if(SMSreceiver.ACTION_SMS_RECEIVED.equals(intent.getAction()))
+      {
+        Log.d(TAG, "Received text message: aborted");
+        return;
+      }
       Bundle extras = intent.getExtras();
 
       if(extras == null)
       {
+        Log.d(TAG, "null intent extras: aborted");
         return;
+      }
+
+      //iterate through all pdus, constructs sms, decrypt contents, push plain
+      for(Object pdu: ((Object[]) extras.get("pdus")))
+      {
+        (Key.getFetcher()).decrypt(
+          (SmsMessage.createFromPdu(pdu)).getUserData());
       }
     }
   }
