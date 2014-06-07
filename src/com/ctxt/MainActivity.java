6805aa905840d6f1875f24a8c3e7df0e9bcbea01
void- <mr.alex.guy@gmail.com>
2014-06-07 15:08:39 -0700

    ui: Adapted MainActivity to use static Fetcher encryption method.

--- src/com/ctxt/MainActivity.java
+++ src/com/ctxt/MainActivity.java
@@ -3,6 +3,7 @@ package com.ctxt;
 import com.share.KeyShare;
 import com.key.Key;
 import com.key.Storer;
+import com.key.Fetcher;
 import com.key.NumberKeyPair;
 import com.key.KeyAlreadyExistsException;
 
@@ -14,10 +15,7 @@ import android.widget.EditText;
 import android.util.Log;
 import android.telephony.SmsManager;
 
-import java.security.*;
-import java.security.spec.X509EncodedKeySpec;
-import java.security.spec.InvalidKeySpecException;
-import javax.crypto.Cipher;
+import java.security.interfaces.RSAPublicKey;
 import java.nio.charset.Charset;
 
 public class MainActivity extends Activity
@@ -38,25 +36,14 @@ public class MainActivity extends Activity
     m = SmsManager.getDefault();
 
     //test ciphers
-    try
-    {
-      PublicKey k = ((Key.getFetcher(getApplicationContext())).shareKey()).
-        getKey();
-      Cipher c = Cipher.getInstance(Storer.CIPHER);
-      c.init(Cipher.ENCRYPT_MODE, k);
-      //test that decryption is encryption inverse
-      Log.d(TAG, "Hello, World ?= " + new
-        String((Key.getStorer(getApplicationContext())).decrypt(
-        c.doFinal("Hello, World!".getBytes()))));
-    }
-    catch(NoSuchAlgorithmException e) {Log.e(TAG, "exception", e); }
-    catch(InvalidKeyException e) {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.NoSuchPaddingException e)
-    {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.IllegalBlockSizeException e)
-    {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.BadPaddingException e)
-    {Log.e(TAG, "exception", e); }
+    //Key.getStorer(getApplicationContext());
+    Key.getFetcher(getApplicationContext());
+    Log.d(TAG,"exp:"+((RSAPublicKey)((Key.getFetcher(getApplicationContext())).shareKey().getKey())).getPublicExponent());
+    //test that decryption is encryption inverse
+    Log.d(TAG, "Hello, World ?= " + new
+      String((Key.getStorer(getApplicationContext())).decrypt(
+      Fetcher.encrypt("Hello, World!".getBytes(),
+      (Key.getFetcher(getApplicationContext())).shareKey().getKey()))));
   }
 
   /**
@@ -76,22 +63,9 @@ public class MainActivity extends Activity
   {
     String msg = (((EditText) findViewById(R.id.msg)).getText()).toString();
     String no = (((EditText) findViewById(R.id.number)).getText()).toString();
-    PublicKey k = ((Key.getFetcher(getApplicationContext())).fetchKey(no)).
-      getKey();
-    try
-    {
-      Cipher c = Cipher.getInstance(Storer.CIPHER);
-      c.init(Cipher.ENCRYPT_MODE, k);
-      byte[] emsg = c.doFinal(msg.getBytes());
-      m.sendDataMessage(no, null, (short) 16101, emsg, null, null);
-    }
-    catch(NoSuchAlgorithmException e) {Log.e(TAG, "exception", e); }
-    catch(InvalidKeyException e) {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.NoSuchPaddingException e)
-    {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.IllegalBlockSizeException e)
-    {Log.e(TAG, "exception", e); }
-    catch(javax.crypto.BadPaddingException e)
-    {Log.e(TAG, "exception", e); }
+
+    m.sendDataMessage(no, null, (short) 16101, Fetcher.encrypt(msg.getBytes(),
+      ((Key.getFetcher(getApplicationContext())).fetchKey(no)).getKey()), null,
+      null);
   }
 }
