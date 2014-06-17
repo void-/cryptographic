406570ba837e0c1f066dc0ef1c2d81eb72e487ff
void- <mr.alex.guy@gmail.com>
2014-06-17 00:44:38 -0700

    docs: Better documented Fetcher.

--- src/com/key/Fetcher.java
+++ src/com/key/Fetcher.java
@@ -24,52 +24,73 @@ import java.io.FileOutputStream;
 import java.io.ByteArrayOutputStream;
 
 /**
- *  Fetcher class given someone's contact number, will return their public key.
+ *  Fetcher class provides an interface for retrieving public keys. Methods are
+ *  provided for handling the user's public key and encryption. Keys are stored
+ *  in (Phone number: Public key) pairs.
+ *
+ *  Terminology:
+ *    The term, "user", refers to the owner of the mobile phone this
+ *    application executes on. The phrase, "user's public key", refers to the
+ *    public key in the (Phone number: Public key), pair with the phone number
+ *    of the mobile phone this application executes on. There is only one user
+ *    per mobile phone.
  *
  *  Example usage:
  *    Fetcher f = new Fetcher();
- *    f.storeSelfKey(new PublicKey());
- *    Storer.NumberKeyPair myKey = f.shareKey();
- *    Storer.NumberKeyPair nkp = f.fetchKey("+1 555 555 5555");
+ *    f.storeSelfKey(new PublicKey(--arguments--));
+ *    NumberKeyPair myKey = f.shareKey();
+ *    NumberKeyPair nkp = f.fetchKey("+1 555 555 5555");
+ *    Fetcher.encrypt("Hello, World!".getBytes(), nkp.getKey());
  *
  *  This entails general method usage, but excludes exception handling and the
- *  use of the Key singleton.
+ *  use of the Key singleton. For actual usage in the rest of the program, get
+ *  a static instance of the Fetcher class via calling Key.getFetcher().
  *
- *  (prospective) General useage steps:
- *    Initialize a Fetcher; it will open up the database or whatever it needs
- *    Ask the Fetcher to enumerate all contacts (people who's pub keys it has)
- *    Ask the Fetcher for someone's public key; raise exception if not found
- *    Ask someone(?) to encrypt a message under the public key
- *    ...other things that dont concern the Fetcher
+ *  The Fetcher class has a particular way to be initialized with respect to
+ *  the Storer class. The method, Storer.generateKeyPair() must be called prior
+ *  to any calls to Fetcher methods. This need only happen once in time, *not*
+ *  each time the program executes. Storer.generateKeyPair() generates a public
+ *  key for the user and requests that a static Fetcher instance store this
+ *  key. Once any instance of the Fetcher class has stored a public key
+ *  corresponding to the user, then all methods provided in this class achieve
+ *  full functionality. Attempting to store a key corresponding to the same
+ *  phone number will result in a KeyAlreadyExistsException. A means to test if
+ *  the user's public key has been stored on disk is to check if shareKey()
+ *  returns null.
  *
- *    or
- *    Init a Fetcher
- *    Inform the Fetcher to store a new public key(why's this a fetcher then?)
- *    Ask the Fetcher for own public key(combine with regular lookup?)
- *
- *
+ *  The means by which Fetcher stores
  */
 public class Fetcher
 {
   /**
    *  Class Variables.
    *
-   *  TAG constant string representing the tag to use when logging events that
-   *    originate from calls to Fetcher methods.
+   *  TAG constant string representing the tag to use when logging exceptions
+   *    and debug statements that originate from calls to Fetcher methods.
    *  NUMBER_STORE string representing the file name used to store the user's
-   *    phone number.
-   *  context Context under which this app operates. This is used for access to
-   *    the filesystem; for key storage.
+   *    phone number. This is necessary because depending on the particular
+   *    mobile phone model, there might be no programmatic way to access the
+   *    phone's phone number. The alternative means is to ask the user to enter
+   *    their phone number and save it on disk.
    */
   private static final String TAG = "FETCHER";
   private static final String NUMBER_STORE = ".myNumber";
-  private static Context context;
 
   /**
-   *  Fetcher() constructs a new Fetcher instance.
+   *  Member Variables.
    *
-   *  If the keystore file does not already exist, a new one will be created.
-   *  Otherwise, the keystore will be loaded from disk.
+   *  context Context under which the application operates. This is used for
+   *    access to the filesystem: for key storage.
+   */
+  private Context context;
+
+  /**
+   *  Fetcher() given a Context, constructs a new Fetcher instance.
+   *
+   *  Nothing special is done. Instantiating a Fetcher does not guarantee that
+   *  this class's methods will have full functionality. This property requires
+   *  that the user's public key has been stored on disk. A means to test for
+   *  this is checking if calling shareKey() returns null.
    */
   Fetcher(Context context)
   {
@@ -77,17 +98,13 @@ public class Fetcher
   }
 
   /**
-   *  enumerateKeys() returns all know public keys as an array of NumberKeyPair
-   *  objects.
+   *  enumerateKeys() returns all phone numbers in the (Phone number: Public
+   *  key) pairs stored by this class.
    *
-   *  If, for some reason, any number in the keystore does not have a
-   *  corresponding public key or does not exist, the entry in the array will
-   *  be null.
+   *  To retrieve the public key that corresponds to a phone number, call
+   *  fetchKey() with an argument of the phone number String.
    *
-   *  Caching is done on the output by private member variable 'savedPairs'.
-   *  The savedPairs cache is invalidated by setting it to null.
-   *
-   *  @return array of NumberKeyPair objects representing (number : key) pairs.
+   *  @return array of Strings containing all stored phone numbers.
    */
   public String[] enumerateKeys()
   {
@@ -102,13 +119,14 @@ public class Fetcher
    *  returned.
    *
    *  @param number number to find the public key for.
-   *  @return (number: number public key) NumberKeyPair instance or null.
+   *  @return (number: number's public key) NumberKeyPair instance or null.
    */
   public NumberKeyPair fetchKey(String number)
   {
     //unify the number to a standard format
     String numberFormatted = PhoneNumberUtils.formatNumber(number);
     FileInputStream f = null;
+    //read the public key from disk
     try
     {
       f = context.openFileInput(numberFormatted);
@@ -139,17 +157,18 @@ public class Fetcher
     {
       Log.e(TAG, "Problem decoding key; might have been modified.", e);
     }
+    //failure to find/read/decode public key
     return null;
   }
 
   /**
    *  shareKey() fetches the user's public key.
    *
-   *  @return user's public key wrapped in a NumberKeyPair.
+   *  @return user's public key in a NumberKeyPair or null if not found.
    */
   public NumberKeyPair shareKey()
   {
-    //load key from disk
+    //load user's phone number from disk
     ByteArrayOutputStream buffer = null;
     try
     {
@@ -178,15 +197,14 @@ public class Fetcher
   }
 
   /**
-   *  newKey() given a phone number and public key will insert it into the
-   *  keystore.
+   *  newKey() given a phone number and public key will persistently store it
+   *  on disk.
+   *
+   *  To store the user's public key, use storeSelfKey() instead.
    *
    *  If the number already has a corresponding public key, regardless of
    *  whether the given public key is different, a KeyAlreadyExistsException is
-   *  raised.
-   *
-   *  Each call the newKey() rewrites the entire keystore file and invalidates
-   *  the enumerateKeys() output cache.
+   *  thrown.
    *
    *  @param number phone number corresponding to the public key to insert.
    *  @param key public key to insert.
@@ -215,17 +233,20 @@ public class Fetcher
   }
 
   /**
-   *  storeSelfKey() given the user's public key will create a new entry in the
-   *  public key keystore under the user's phone number.
+   *  storeSelfKey() given the user's phone number and public key will
+   *  persistently store the public key and phone number (the user's phone
+   *  number will appear twice on disk).
    *
    *  If the user's public key is already an entry, a KeyAlreadyExistsException
    *  is thrown.
    *
-   *  @param key user's public key.
+   *  @param number String representing the user's mobile phone number.
+   *  @param key PublicKey representing the user's public key.
    */
   void storeSelfKey(String number, PublicKey key) throws
     KeyAlreadyExistsException
   {
+    //store the key like normal; newKey will properly format the number
     this.newKey(number, key);
     //write phone number to file
     try
@@ -245,11 +266,16 @@ public class Fetcher
   }
 
   /**
-   *  encrypt() given plaintext, produces ciphertext.
+   *  encrypt() given a plaintext, produces the corresponding ciphertext
+   *  encrypted under a given public key.
+   *
+   *  null is returned if the encryption was unsuccessful. However, an
+   *  unchecked exception is probably raised if the given plaintext byte array
+   *  exceeds the amount of data the encryption algorithm can accept.
    *
    *  @param plaintext plaintext byte array to encrypt.
    *  @param k public key to encrypt under.
-   *  @return ciphertext of plaintext under public key k.
+   *  @return ciphertext of plaintext encrypted under public key k or null.
    */
   public static byte[] encrypt(byte[] plaintext, PublicKey k)
   {
